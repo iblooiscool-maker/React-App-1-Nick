fd10529bd71cc4daf73edcf2f7c9c1383ee44003
Nicholas Valladares <iblooiscool@gmail.com>
2026-02-11 19:34:13 -0500

    Complete TODOs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,27 +4,49 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
+
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
+
   let [filter, setFilter] = useState("")
+
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
     console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+    // TODO: 3. Add a new menu item to the correct variable associated with this class.
+    // This involves adding a parameter and changing a class instance variable (props).
+    const text = newMenuItem.trim()
+    if (!text) return
+
+    setMenuItems((prev) => [...prev, text])
+    setNewMenuItem("")
+  }, [newMenuItem])
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
+  function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+  }
+
+  let filteredMenuItems = menuItems
+  if (filter) {
+    const regex = new RegExp(escapeRegExp(filter), "i")
+    filteredMenuItems = menuItems.filter((item) => regex.test(item))
+  }
 
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
   return (
     <div>
+      <ul>
+        {filteredMenuItems.map((item, idx) => (
+          <li key={`${item}-${idx}`}>{item}</li>
+        ))}
+      </ul>
+
       <input
         type="text"
         id="newMenuItemValue"
@@ -35,6 +57,7 @@ export default function Sidebar() {
       <button
         onClick={() => {
           /* TODO: 3 */
+          addMenuItem()
         }}
       >
         Add Item
